379f296dfa1ee49728c943ef09a267ac66efdc09
wchengong <wchengong@it-mmc16-m12.eastern.ewu.edu>
2016-12-02 17:32:23 -0800

    2

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = '/img/fullsize/1.jpg';
-var img1 = '/img/fullsize/2.jpg';
-var img2 = '/img/fullsize/3.jpg';
+var img0 = '/img/fullsize/MUJI1.jpg';
+var img1 = '/img/fullsize/WN6.jpg';
+var img2 = '/img/fullsize/MUJI5.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
