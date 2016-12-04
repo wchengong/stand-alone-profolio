fc33317e993a4e11bed508c6c539643a399f415f
wchengong <wchengong@it-mmc16-m07.eastern.ewu.edu>
2016-12-03 17:18:21 -0800

    5

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = '/img/fullsize/logo.jpg';
-var img1 = '/img/fullsize/collgraph.jpg';
-var img2 = '/img/fullsize/MUJI5.jpg';
+var img0 = '/img/forheader.jpg';
+var img1 = '/img/forheader 2.jpg';
+var img2 = '/img/forheader 3.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
