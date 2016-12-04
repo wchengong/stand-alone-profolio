b5e783a65ab1163db1247f26d913f59786a49e97
wchengong <wenjuan.chengong@gmail.com>
2016-12-03 17:55:52 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = 'stand-alone-profolio/img/forheader.jpg';
-var img1 = 'stand-alone-profolio/img/forheader 2.jpg';
-var img2 = 'stand-alone-profolio/img/forheader 3.jpg';
+var img0 = '/stand-alone-profolio/img/forheader.jpg';
+var img1 = '/stand-alone-profolio/img/forheader 2.jpg';
+var img2 = '/stand-alone-profolio/img/forheader 3.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
